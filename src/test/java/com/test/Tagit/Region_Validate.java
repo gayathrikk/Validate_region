e7d20d9b0795dff5e8adacdf723fe8898ee09d61
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2025-03-27 13:16:33 +0530

    Update Region_Validate.java

--- src/test/java/com/test/Tagit/Region_Validate.java
+++ src/test/java/com/test/Tagit/Region_Validate.java
@@ -54,7 +54,7 @@ public class Region_Validate {
 		WebDriverWait wait2 = new WebDriverWait(driver, 20);
 		WebElement emailInput = wait2
 				.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@type='email']")));
-		emailInput.sendKeys("softwaretestingteam9@gmail.com");
+		emailInput.sendKeys("teamsoftware457@gmail.com");
 		System.out.println("Mail I'd is entered");
 		WebDriverWait wait3 = new WebDriverWait(driver, 20);
 		WebElement Next = wait3.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Next']")));
